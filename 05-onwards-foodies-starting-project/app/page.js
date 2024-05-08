af3776e17c8dc83322d05081d59c49d9bf7ebecd
KBandipo~ <keneebandipo@gmail.com>
2024-05-08 12:28:06 +0100

    Remove some lines of code

--- 05-onwards-foodies-starting-project/app/page.js
+++ 05-onwards-foodies-starting-project/app/page.js
@@ -1,23 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
-      <p>
-        <Link href="/meals">Meal Page</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meal Page</Link>
-      </p>
-      <p>
-        <Link href="/community">Community Page</Link>
-      </p>
-      <p>
-        <Link href="/meals/hjhhj">Meal Page</Link>
-      </p>
     </main>
   );
 }
